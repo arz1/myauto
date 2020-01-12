2bdd26a4c5f292c6c2a870a174f889cd43875c60
Adam <adam@adam-PC>
2020-01-12 21:56:05 +0100

    Added WSL installation.

---
+++ userbash.ps1
@@ -0,0 +1,14 @@
+Enable-WindowsOptionalFeature -NoRestart -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+#Force restart
+# After restart
+#If the download is taking a long time, turn off the progress bar by setting 
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile WslUbuntu1804.zip -UseBasicParsing
+Expand-Archive -Path WslUbuntu1804.zip -DestinationPath WslUbuntu1804
+cd WslUbuntu1804
+.\ubuntu1804.exe install --root
+wsl
+useradd -m -p $(openssl passwd -crypt a) -s /bin/bash wsluser
+usermod -aG sudo wsluser
+exit
+.\ubuntu1804.exe config --default-user wsluser
